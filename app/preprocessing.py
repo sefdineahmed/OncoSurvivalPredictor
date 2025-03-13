12a6f1aa9086c4c0f9361ac28f0c5c75093db86e
Ahmed Sefdine <sefdine668@gmail.com>
2025-03-13 11:50:23 +0000

    Update preprocessing.py
    Signed-off-by: Ahmed Sefdine <sefdine668@gmail.com>

--- app/preprocessing.py
+++ app/preprocessing.py
@@ -1,14 +1,12 @@
 import pandas as pd
 
+
 def preprocess_data(data):
-    """Prétraitement des données avant la prédiction."""
-    
-    # Conversion des variables catégorielles en variables numériques
-    data['SEXE'] = data['SEXE'].map({'Homme': 1, 'Femme': 0})
+    # Exemple de prétraitement pour les variables binaires
     data['Cardiopathie'] = data['Cardiopathie'].map({'Oui': 1, 'Non': 0})
     data['Ulceregastrique'] = data['Ulceregastrique'].map({'Oui': 1, 'Non': 0})
     data['Douleurepigastrique'] = data['Douleurepigastrique'].map({'Oui': 1, 'Non': 0})
-    data['Ulcerobourgeonnant'] = data['Ulcerobourgeonnant'].map({'Oui': 1, 'Non': 0})
+    data['Ulcero-bourgeonnant'] = data['Ulcero-bourgeonnant'].map({'Oui': 1, 'Non': 0})  # Correction ici
     data['Denitrution'] = data['Denitrution'].map({'Oui': 1, 'Non': 0})
     data['Tabac'] = data['Tabac'].map({'Oui': 1, 'Non': 0})
     data['Mucineux'] = data['Mucineux'].map({'Oui': 1, 'Non': 0})
@@ -16,8 +14,4 @@ def preprocess_data(data):
     data['Stenosant'] = data['Stenosant'].map({'Oui': 1, 'Non': 0})
     data['Metastases'] = data['Metastases'].map({'Oui': 1, 'Non': 0})
     data['Adenopathie'] = data['Adenopathie'].map({'Oui': 1, 'Non': 0})
-
-    # Remplacer les valeurs NaN par la médiane (au cas où certaines valeurs sont manquantes)
-    data.fillna(data.median(), inplace=True)
-
     return data
